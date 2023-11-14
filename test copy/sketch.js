4faffe1fa6b45df97397ac9da55d85e7de2c2b53
sarahmaisha <smaisha285@gmail.com>
2023-11-14 16:41:27 -0700

    fixed errors

--- test copy/sketch.js
+++ test copy/sketch.js
@@ -6,6 +6,7 @@ let imageSize = 60;
 let xOffset = 0.0;
 let yOffset = 0.0;
 let correctChoices = 0;
+//let rectLocked = [];
 let gColorX = 600; // X-coordinate at which color changes
 let gColorY = 100;
 let rColorY = 200;
@@ -16,11 +17,12 @@ let startTime = 0;
 let elapsedTime = 0;
 let isTiming = false;
 let gameCompleted = false;
-
+let isDrawing=false;
+let greenCounter = 0;
 
 function preload(){
  // load images
- 
+ hand=loadImage("images/hand.png");
  correctNoise = loadSound("noise/ding.mp3");
  wrongNoise = loadSound("noise/wrong.mp3");
  title = loadImage("images/title.png");
@@ -29,7 +31,8 @@ function preload(){
  sortVeggies = loadImage("images/image-4.png");
  returnToMenu = loadImage("images/return.png");
  cutThePizza = loadImage("images/CutThePizza.png");
- washTheDishes = loadImage("images/washTheDishes.png");
+ washTheDishes = loadImage("images/WashDishes.png");
+ dishscreen= loadImage("images/WashTheDishes.png");
  veggies1= loadImage("images/veggies_0.png");
  veggies2= loadImage("images/veggies_1.png");
  veggies3= loadImage("images/veggies_2.png");
@@ -37,12 +40,8 @@ function preload(){
  veggies5= loadImage("images/veggies_4.png");
  veggies6= loadImage("images/veggies_5.png");
  veggiesscreen= loadImage("images/Sorting_Vegetables.png");
- greenBasket = loadImage("images/greenBasket.png");
- redBasket = loadImage("images/redBasket.png");
- restart = loadImage("images/restart.png");
- goodJob = loadImage("images/good_job.png");
- cloud = loadImage("images/cloud.png");
  b1=loadImage("images/basket.png");
+ b2=loadImage("images/basket.png");
  wrongVeggie = loadImage("images/wrongVeggie.png");
  rightVeggie = loadImage("images/correctVeggie.png");
 
@@ -82,6 +81,8 @@ images.push({img: veggies6, initialX: 300, initialY: 200, x: 300, y: 200,
 function setup() {
  createCanvas(800, 400);
  startTime = millis();
+ strokeWeight(20);
+ noFill();
  /*for (let i = 0; i < 6; i++) {
    rectangles.push({
      x: 75,
@@ -96,6 +97,7 @@ function setup() {
 }
 
 function draw(){
+ 
  if (screen==0){
    menuscreen();
  }
@@ -111,14 +113,36 @@ function draw(){
  else if (screen==4){
    cutPizza();
  }
- else {
-   dishes();
- }
- }
+ else if (isDrawing){
+  background(dishscreen);
+  const distance = dist(mouseX, mouseY, 220, 220);
+  const greenMargin = 125; // 7-pixel margin of error added to the radius
+
+  if (distance <= greenMargin) {
+    stroke('blue'); // Set the line green
+    greenCounter++;
+  } else {
+    stroke(255, 0, 0); // Set the line red
+  }
+
+  line(pmouseX, pmouseY, mouseX, mouseY);
+
+  // Check if the greenCounter is greater than or equal to a certain threshold
+  if (greenCounter >= 0.95 * 125) {
+    // Stop the drawing process by setting isDrawing to false
+    isDrawing = false;
+    textSize(32);
+    fill(0);
+    text("Good job!", width / 2, height / 2);
+  }
+}
+
+}
 
 
  function mousePressed() {
-   for (let i = 0; i < images.length; i++) {
+   if (screen==3){
+  for (let i = 0; i < images.length; i++) {
      let touching =
        mouseX > images[i].x &&
        mouseX < images[i].x + imageSize &&
@@ -132,17 +156,28 @@ function draw(){
        yOffset = mouseY - images[i].y;
      }
    }
+  }
+  else if (screen==5){
+    isDrawing = true;
+  }
  }
   function mouseDragged() {
+   
+    if (screen==3){
     if(selectedImage>=0){
       images[selectedImage].x = mouseX - xOffset;
       images[selectedImage].y = mouseY - yOffset;
     }
-
+  }
+  
  }
   function mouseReleased() {
+    if (screen==3){
     selectedImage = -1;
-  
+    }
+    else if (screen==5){
+      isDrawing = false;
+    }
  }
 
 
@@ -245,9 +280,10 @@ if(mouseIsPressed==true&& mouseX>=320&&mouseX<=490&&mouseY>=220&&mouseY<=280){
    // Display the elapsed time when all vegetables are correctly placed
    if (allCorrect) {
      fill(0); // Set text color to black
-     textSize(15);
-     image(cloud,30,250,230,180);
-     text(`Time: ${Math.floor(elapsedTime / 1000)} seconds`, 60, 350);
+     textSize(20);
+     text(`Time: ${Math.floor(elapsedTime / 1000)} seconds`, 200, 320);
+     
+    
    }
   allCorrect = true; // Assume all vegetables are in the correct place
 
@@ -255,12 +291,12 @@ if(mouseIsPressed==true&& mouseX>=320&&mouseX<=490&&mouseY>=220&&mouseY<=280){
     let inGreen = images[i].x > gColorX - 20 &&
       images[i].x < gColorX + 120 &&
       images[i].y > gColorY - 20 &&
-      images[i].y < gColorY + 80 &&
+      images[i].y < gColorY + 120 &&
       !images[i].pickedRight;
 
     let inRed = images[i].x > gColorX - 20 &&
       images[i].x < gColorX + 120 &&
-      images[i].y > rColorY +20&&
+      images[i].y > rColorY - 20 &&
       images[i].y < rColorY + 120 &&
       !images[i].pickedRight;
 
@@ -272,9 +308,7 @@ if(mouseIsPressed==true&& mouseX>=320&&mouseX<=490&&mouseY>=220&&mouseY<=280){
       images[i].pickedRight = true;
       correctChoices++;
       correctMessage = "Correct!";
-      correctNoise.play();
     } else if (inGreen && images[i].rg == "r") {
-      wrongNoise.play();
       images[i].tryAgain = true;
       allCorrect = false; // At least one vegetable is in the wrong place
     }
@@ -287,9 +321,7 @@ if(mouseIsPressed==true&& mouseX>=320&&mouseX<=490&&mouseY>=220&&mouseY<=280){
       images[i].pickedRight = true;
       correctChoices++;
       correctMessage = "Correct!";
-      correctNoise.play();
     } else if (inRed && images[i].rg == "g") {
-      wrongNoise.play();
       images[i].tryAgain = true;
       allCorrect = false; // At least one vegetable is in the wrong place
     }
@@ -299,22 +331,21 @@ if(mouseIsPressed==true&& mouseX>=320&&mouseX<=490&&mouseY>=220&&mouseY<=280){
     }
   }
 
-  
+  text("green basket", 600, 80);
   image(b1, 600, 100, 100, 100);
-  image(greenBasket, 615, 150, 65, 40);
 
-
-  image(b1, 600, 250, 100, 100);
-  image(redBasket,615,300,65,40);
+  text("red basket", 600, 230);
+  image(b2, 600, 250, 100, 100);
 
   for (let i = 0; i < images.length; i++) {
     if (images[i].tryAgain) {
-      //wrongNoise.play();
+      
       fill(255, 0, 0);
      // text("Try again...", 500, 200);
       image(wrongVeggie, 500, 130, 100,100);
 
       images[i].tryAgain = false;
+      wrongNoise.play();
     }
   }
 
@@ -322,8 +353,8 @@ if(mouseIsPressed==true&& mouseX>=320&&mouseX<=490&&mouseY>=220&&mouseY<=280){
   //text(correctMessage, 500, 200);
   // Clear the "Correct!" message after a delay
   if (correctMessage === "Correct!") {
-   //correctNoise.play();
   image(rightVeggie, 500,170,100,100);
+   correctNoise.play();
     setTimeout(function () {
       correctMessage = "";
     }, 1000); // Adjust the duration (in milliseconds) as needed
@@ -333,16 +364,19 @@ if(mouseIsPressed==true&& mouseX>=320&&mouseX<=490&&mouseY>=220&&mouseY<=280){
   // If all vegetables are in the correct place, display "Good Job!"
   if (allCorrect && correctChoices === images.length) {
     fill(0, 255, 0);
-    image(goodJob, 250, 150,200,150);
+    text("Good Job!", 350, 200);
     fill(0); // Set text color to black
     textSize(20);
-   // text("Time: " + Math.floor(elapsedTime / 1000) + " seconds", 350, 240);
+    text("Time: " + Math.floor(elapsedTime / 1000) + " seconds", 350, 240);
     
-    //restart
-    image(restart,330,320,150,90);
+    // Display a restart button
+    fill(255);
+    rect(390, 280, 100, 40);
+    fill(0);
+    text("Restart", 360, 290);
 
     // Check if the restart button is clicked
-    if (mouseIsPressed && mouseX >= 330 && mouseX <= 480 && mouseY >= 320 && mouseY <= 410) {
+    if (mouseIsPressed && mouseX >= 360 && mouseX <= 490 && mouseY >= 280 && mouseY <= 320) {
       resetVeggies();
     }
 
@@ -371,6 +405,7 @@ for (let i = 0; i < images.length; i++) {
   images[i].y = images[i].initialY;
   images[i].pickedRight = false;
   images[i].canMove = true;
+}
   }
 
  
@@ -389,14 +424,7 @@ for (let i = 0; i < images.length; i++) {
  }
  }
   function dishes(){
-    background('magenta');
- textSize(50);
- fill('maroon');
- image(title,330,100,200,200);
- image(returnToMenu, 640,340,150,50);
- if(mouseIsPressed==true&& mouseX>=640&&mouseX<=790&&mouseY>=340&&mouseY<=390){
-   selectgame();
- }
+ 
  }
  
  function homepage(){
@@ -418,6 +446,3 @@ for (let i = 0; i < images.length; i++) {
    screen=5;
  }
  
-
-
-  }
